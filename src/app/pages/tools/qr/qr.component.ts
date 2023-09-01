a66dc36bedc54ef7e780f53a6ca7de335529c618
nam <nam@channel-it.com>
2023-09-01 16:16:58 +0300

    higher quality qr library

--- src/app/pages/tools/qr/qr.component.ts
+++ src/app/pages/tools/qr/qr.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 
-import * as qr from 'qrcode';
+import * as QRCode from 'qrcode-svg';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-qr',
@@ -18,21 +19,48 @@ export class QrComponent {
       this.ts.error('Please enter a url');
       return;
     }
-    qr.toDataURL(this.urlInput, (err, url) => {
-      if (err) {
-        console.error(err);
-      } else {
-        this.downloadQRCode(url);
-      }
+
+    const qr = new QRCode({
+      content: this.urlInput,
+      width: 1000, // Adjust the width for higher resolution
+      height: 1000, // Adjust the height for higher resolution
     });
+
+    const svgString = qr.svg();
+
+    // Convert SVG to PNG and download
+    this.downloadQRCode(svgString);
   }
 
-  downloadQRCode(dataURL: string) {
-    const link = document.createElement('a');
-    link.href = dataURL;
-    link.download = 'qr-code.png';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  downloadQRCode(svgString: string) {
+    const blob = new Blob([svgString], { type: 'image/svg+xml' });
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      const dataURL = reader.result as string;
+
+      // Convert SVG to PNG using a hidden canvas
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+      const img = new Image();
+
+      img.onload = () => {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx?.drawImage(img, 0, 0);
+
+        // Trigger the download of the PNG
+        canvas.toBlob((blob) => {
+          if (blob) {
+            saveAs(blob, 'qr-code.png');
+          }
+        }, 'image/png');
+      };
+
+      img.src =
+        'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(svgString);
+    };
+
+    reader.readAsText(blob);
   }
 }
