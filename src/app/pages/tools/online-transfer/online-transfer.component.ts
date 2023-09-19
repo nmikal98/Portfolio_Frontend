d3318735fa8d0f4214a875d7d67fd6b0c0ebbf56
nam <nam@channel-it.com>
2023-09-19 10:46:35 +0300

    .

--- src/app/pages/tools/online-transfer/online-transfer.component.ts
+++ src/app/pages/tools/online-transfer/online-transfer.component.ts
@@ -41,11 +41,11 @@ export class OnlineTransferComponent {
       if (allowedFileTypes.includes(file.type)) {
         const fileSizeInMB = file.size / (1024 * 1024);
 
-        if (fileSizeInMB <= 100) {
+        if (fileSizeInMB <= 20) {
           this.fileToUpload = file;
         } else {
           this.fileToUpload = null;
-          this.ts.error('Please upload a file smaller than 100MB');
+          this.ts.error('Please upload a file smaller than 20MB');
         }
       } else {
         this.fileToUpload = null;
